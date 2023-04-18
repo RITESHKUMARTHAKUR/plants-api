1dd24ae9d32772ad0a9a06a89095bf6b6b14f3d5
RITESHKUMARTHAKUR <thakurram2668@gmail.com>
2023-04-18 08:33:10 +0530

    added search filter

--- controllers/products.js
+++ controllers/products.js
@@ -2,14 +2,32 @@ const Plant = require("../models/product");
 
 
 const getAllPlants = async (req, res) => {
-    const { size, commonname  } = req.query;
+    const { name, size, commonname,category,space,sunlight,temperature,watering  } = req.query;
     const queryObject = {};
+    if (name) {
+        queryObject.name = { $regex: name, $options: "i" };
+    }
 
     if (size) {
         queryObject.size = { $regex: size, $options: "i" };
     }
     if (commonname) {
         queryObject.commonname = { $regex: commonname, $options: "i" };
+    }
+    if (category) {
+        queryObject.category = { $regex: category, $options: "i" };
+    }
+    if (space) {
+        queryObject.space = { $regex: space, $options: "i" };
+    }
+    if (sunlight) {
+        queryObject.sunlight = { $regex: sunlight, $options: "i" };
+    }
+    if (temperature) {
+        queryObject.temperature = { $regex: temperature, $options: "i" };
+    }
+    if (watering) {
+        queryObject.watering = { $regex: watering, $options: "i" };
     }
 
     const plants = await Plant.find(queryObject)
--- plantDB.js
+++ plantDB.js
@@ -7,7 +7,7 @@ const PlantsJson = require("./plants.json")
 const start = async () => {
     try {
         await connectDB(process.env.MONGODB_URL);
-        await Plants.deleteMany();
+        // await Plants.deleteMany();
         await Plants.create(PlantsJson);
         console.log("Success")
     }catch (error) {
